7d07582376d53b54a6bd0dc7cc32a29a83c4f7e8
JSellinger <132168510+JSellinger@users.noreply.github.com>
2024-12-05 20:19:32 -0600

    Update app.py
    Datagrid ui title card changed and general title changed

--- app/app.py
+++ app/app.py
@@ -7,7 +7,7 @@ import palmerpenguins
 
 df = palmerpenguins.load_penguins()
 
-ui.page_opts(title="Penguins dashboard", fillable=True)
+ui.page_opts(title="Palmer Penguins Data Dashboard", fillable=True)
 
 
 with ui.sidebar(title="Filter controls"):
@@ -22,17 +22,17 @@ with ui.sidebar(title="Filter controls"):
     ui.h6("Links")
     ui.a(
         "GitHub Source",
-        href="https://github.com/denisecase/cintel-07-tdash",
+        href="https://github.com/JSellinger/cintel-07-tdash",
         target="_blank",
     )
     ui.a(
         "GitHub App",
-        href="https://denisecase.github.io/cintel-07-tdash/",
+        href="https://JSellinger.github.io/cintel-07-tdash/",
         target="_blank",
     )
     ui.a(
         "GitHub Issues",
-        href="https://github.com/denisecase/cintel-07-tdash/issues",
+        href="https://github.com/JSellinger/cintel-07-tdash/issues",
         target="_blank",
     )
     ui.a("PyShiny", href="https://shiny.posit.co/py/", target="_blank")
@@ -85,7 +85,7 @@ with ui.layout_columns():
             )
 
     with ui.card(full_screen=True):
-        ui.card_header("Penguin da")
+        ui.card_header("Penguin Filterable Data Frame")
 
         @render.data_frame
         def summary_statistics():
